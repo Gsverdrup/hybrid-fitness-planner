5e796d1852aeb1ac576e56b0a83081ca184d846d
Gunnar Sverdrup <gsverdrup21@gmail.com>
2025-12-19 15:28:35 -0800

    backend: express + postgres connected

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import usersRouter from "./routes/users";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/users", usersRouter);
 
 app.get('/health', (_, res) => {
   res.json({ status: 'ok' });
